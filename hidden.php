b775aacc2d823ae73dc92424f78b463db007c6bd
Zack Katz <zack@katzwebservices.com>
2017-02-01 16:09:39 -0700

    Link to GPL

--- hidden.php
+++ hidden.php
@@ -8,23 +8,8 @@ Author URI: http://www.katzwebservices.com
 Version: 2.0
 Text Domain: cf7_modules
 Domain Path: languages
-*/
-
-/*  Copyright 2015 Katz Web Services, Inc. (email: info at katzwebservices.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 add_action('init', 'contact_form_7_hidden_fields_textdomain');
